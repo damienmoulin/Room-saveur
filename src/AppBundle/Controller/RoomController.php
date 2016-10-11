0408204508b2cdd739b5220df2e64d87f8992f07
damienmoulin <damien.moulin1@gmail.com>
2016-10-11 17:12:58 +0200

    [Room] Edit

--- src/AppBundle/Controller/RoomController.php
+++ src/AppBundle/Controller/RoomController.php
@@ -62,6 +62,32 @@ class RoomController extends Controller
     }
 
     /**
+     * @param Request $request
+     * @param Room $room
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse|\Symfony\Component\HttpFoundation\Response
+     * @Route("/edit/{id}", name="appbundle_dashboard_room_edit")
+     */
+    public function editRoomAction(Request $request, Room $room)
+    {
+        $form = $this->get('form.factory')->create('AppBundle\Form\RoomType', $room);
+
+        $form->handleRequest($request);
+
+        if ($form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($room);
+            $em->flush();
+
+            return $this->redirect($this->generateUrl('appbundle_dashboard_room'));
+        }
+
+        return $this->render('Dashboard/Room/add.html.twig',
+            [
+                'form' => $form->createView()
+            ]);
+    }
+
+    /**
      * @param Room $room
      * @return \Symfony\Component\HttpFoundation\RedirectResponse
      * @Route("/remove/{id}",name="appbundle_dashboard_room_remove")
