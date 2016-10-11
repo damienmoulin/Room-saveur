da86f3afa81611f74ce24bfd47cf824795bea2b6
damienmoulin <damien.moulin1@gmail.com>
2016-10-11 14:20:17 +0200

    [FOS] Create User

---
+++ src/AppBundle/Entity/User.php
@@ -0,0 +1,180 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: damien
+ * Date: 11/10/16
+ * Time: 13:57
+ */
+
+namespace AppBundle\Entity;
+
+use FOS\UserBundle\Model\User as BaseUser;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="user")
+ */
+class User extends BaseUser
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    /**
+     * @ORM\Column(name="firstname", type="string", length=45, nullable=true)
+     */
+    private $firstname;
+
+    /**
+     * @ORM\Column(name="lastname", type="string", length=45, nullable=true)
+     */
+    private $lastname;
+
+    /**
+     * @var $company
+     * @ORM\Column(name="company", type="string", length=255, nullable=true)
+     */
+    private $company;
+
+    /**
+     * @var $phone
+     * @ORM\Column(name="phone", type="string", length=10, nullable=true)
+     */
+    private $phone;
+
+    /**
+     * @var $userStatus
+     * @ORM\Column(name="userStatus", type="integer", nullable=true)
+     */
+    private $userStatus;
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Set firstname
+     *
+     * @param string $firstname
+     *
+     * @return User
+     */
+    public function setFirstname($firstname)
+    {
+        $this->firstname = $firstname;
+
+        return $this;
+    }
+
+    /**
+     * Get firstname
+     *
+     * @return string
+     */
+    public function getFirstname()
+    {
+        return $this->firstname;
+    }
+
+    /**
+     * Set lastname
+     *
+     * @param string $lastname
+     *
+     * @return User
+     */
+    public function setLastname($lastname)
+    {
+        $this->lastname = $lastname;
+
+        return $this;
+    }
+
+    /**
+     * Get lastname
+     *
+     * @return string
+     */
+    public function getLastname()
+    {
+        return $this->lastname;
+    }
+
+    /**
+     * Set company
+     *
+     * @param string $company
+     *
+     * @return User
+     */
+    public function setCompany($company)
+    {
+        $this->company = $company;
+
+        return $this;
+    }
+
+    /**
+     * Get company
+     *
+     * @return string
+     */
+    public function getCompany()
+    {
+        return $this->company;
+    }
+
+    /**
+     * Set phone
+     *
+     * @param string $phone
+     *
+     * @return User
+     */
+    public function setPhone($phone)
+    {
+        $this->phone = $phone;
+
+        return $this;
+    }
+
+    /**
+     * Get phone
+     *
+     * @return string
+     */
+    public function getPhone()
+    {
+        return $this->phone;
+    }
+
+    /**
+     * Set userStatus
+     *
+     * @param integer $userStatus
+     *
+     * @return User
+     */
+    public function setUserStatus($userStatus)
+    {
+        $this->userStatus = $userStatus;
+
+        return $this;
+    }
+
+    /**
+     * Get userStatus
+     *
+     * @return integer
+     */
+    public function getUserStatus()
+    {
+        return $this->userStatus;
+    }
+}
