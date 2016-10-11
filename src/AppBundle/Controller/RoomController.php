05a456af4442d5d7274d7e89b514749d5e3a431e
damienmoulin <damien.moulin1@gmail.com>
2016-10-11 15:20:11 +0200

    [Room] add/list/remove

---
+++ src/AppBundle/Controller/RoomController.php
@@ -0,0 +1,79 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: damien
+ * Date: 11/10/16
+ * Time: 14:38
+ */
+
+namespace AppBundle\Controller;
+
+use AppBundle\Entity\Room;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+/**
+ * @Route("/dashboard/room", name="appbundle_dashboard_room")
+ */
+class RoomController extends Controller
+{
+    /**
+     * @return \Symfony\Component\HttpFoundation\Response
+     * @Route("", name="appbundle_dashboard_room")
+     */
+    public function listRoomAction()
+    {
+        $rooms = $this->getDoctrine()->getRepository('AppBundle:Room')
+            ->findAll();
+
+        return $this->render('Dashboard/Room/index.html.twig',
+            [
+                'rooms' => $rooms
+            ]);
+    }
+
+    /**
+     * @param Request $request
+     * @return \Symfony\Component\HttpFoundation\Response
+     * @Route("/add", name="appbundle_dashboard_room_add")
+     */
+    public function addRoomAction(Request $request)
+    {
+        $room = new Room();
+
+        $form = $this->get('form.factory')->create('AppBundle\Form\RoomType', $room);
+
+        $form->handleRequest($request);
+
+        if ($form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($room);
+            $em->flush();
+
+            return $this->redirect($this->generateUrl('appbundle_dashboard_room'));
+        }
+
+        return $this->render('Dashboard/Room/add.html.twig',
+            [
+                'form' => $form->createView()
+            ]);
+
+    }
+
+    /**
+     * @param Room $room
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse
+     * @Route("/remove/{id}",name="appbundle_dashboard_room_remove")
+     */
+    public function removeRoomAction(Room $room)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $em->remove($room);
+        $em->flush();
+
+        return $this->redirect($this->generateUrl('appbundle_dashboard_room'));
+    }
+
+
+}
---
+++ src/AppBundle/Entity/Room.php
@@ -0,0 +1,155 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: damien
+ * Date: 11/10/16
+ * Time: 14:32
+ */
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="room")
+ */
+class Room
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    /**
+     * @var $address
+     * @ORM\Column(name="address", type="string", length=255, nullable=false)
+     */
+    private $address;
+
+    /**
+     * @var $address
+     * @ORM\Column(name="city", type="string", length=255, nullable=false)
+     */
+    private $city;
+
+    /**
+     * @var $address
+     * @ORM\Column(name="postal_code", type="string", length=5, nullable=false)
+     */
+    private $postalCode;
+
+    /**
+     * @var $address
+     * @ORM\Column(name="address_complement", type="string", length=255, nullable=true)
+     */
+    private $addressComplement;
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set address
+     *
+     * @param string $address
+     *
+     * @return Room
+     */
+    public function setAddress($address)
+    {
+        $this->address = $address;
+
+        return $this;
+    }
+
+    /**
+     * Get address
+     *
+     * @return string
+     */
+    public function getAddress()
+    {
+        return $this->address;
+    }
+
+    /**
+     * Set city
+     *
+     * @param string $city
+     *
+     * @return Room
+     */
+    public function setCity($city)
+    {
+        $this->city = $city;
+
+        return $this;
+    }
+
+    /**
+     * Get city
+     *
+     * @return string
+     */
+    public function getCity()
+    {
+        return $this->city;
+    }
+
+    /**
+     * Set postalCode
+     *
+     * @param string $postalCode
+     *
+     * @return Room
+     */
+    public function setPostalCode($postalCode)
+    {
+        $this->postalCode = $postalCode;
+
+        return $this;
+    }
+
+    /**
+     * Get postalCode
+     *
+     * @return string
+     */
+    public function getPostalCode()
+    {
+        return $this->postalCode;
+    }
+
+    /**
+     * Set addressComplement
+     *
+     * @param string $addressComplement
+     *
+     * @return Room
+     */
+    public function setAddressComplement($addressComplement)
+    {
+        $this->addressComplement = $addressComplement;
+
+        return $this;
+    }
+
+    /**
+     * Get addressComplement
+     *
+     * @return string
+     */
+    public function getAddressComplement()
+    {
+        return $this->addressComplement;
+    }
+}
