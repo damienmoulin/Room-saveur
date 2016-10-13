66544bd653a88a89ddfd99e292316cae5bf7abbb
damienmoulin <damien.moulin1@gmail.com>
2016-10-13 12:08:40 +0200

    [Templating] Dashboard/Product

---
+++ src/AppBundle/Controller/DashboardController.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: damien
+ * Date: 13/10/16
+ * Time: 10:24
+ */
+
+namespace AppBundle\Controller;
+
+use AppBundle\Entity\Like;
+use AppBundle\Entity\Product;
+use Symfony\Component\HttpFoundation\Request;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+
+/**
+ * @Route("/dashboard", name="appbundle_dashboard")
+ */
+class DashboardController extends Controller
+{
+    /**
+     * @Route("", name="appbundle_dashboard")
+     */
+    public function indexAction()
+    {
+        return $this->render('Dashboard/index.html.twig');
+    }
+}
--- src/AppBundle/Form/ProductType.php
+++ src/AppBundle/Form/ProductType.php
@@ -2,8 +2,11 @@
 
 namespace AppBundle\Form;
 
+
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -15,12 +18,48 @@ class ProductType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('reference')
-            ->add('designation')
-            ->add('descriptive')
-            ->add('price')
-            ->add('tva')
-            ->add('dlc')
+            ->add('reference',TextType::class,
+                [
+                    'attr' =>
+                        [
+                            'class' => 'form-control'
+                        ]
+                ])
+            ->add('designation',TextareaType::class,
+                [
+                    'attr' =>
+                        [
+                            'class' => 'form-control'
+                        ]
+                ])
+            ->add('descriptive',TextType::class,
+                [
+                    'attr' =>
+                        [
+                            'class' => 'form-control'
+                        ]
+                ])
+            ->add('price',TextType::class,
+                [
+                    'attr' =>
+                        [
+                            'class' => 'form-control'
+                        ]
+                ])
+            ->add('tva',TextType::class,
+                [
+                    'attr' =>
+                        [
+                            'class' => 'form-control'
+                        ]
+                ])
+            ->add('dlc',TextType::class,
+                [
+                    'attr' =>
+                        [
+                            'class' => 'form-control'
+                        ]
+                ])
             ->add('picture', FileType::class)
         ;
     }
