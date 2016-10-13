ae666ba99480b4468874063bb77e2120a9dbee09
damienmoulin <damien.moulin1@gmail.com>
2016-10-13 13:39:44 +0200

    [Room] Templating

--- src/AppBundle/Form/RoomType.php
+++ src/AppBundle/Form/RoomType.php
@@ -5,6 +5,8 @@ namespace AppBundle\Form;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+
 
 class RoomType extends AbstractType
 {
@@ -14,10 +16,46 @@ class RoomType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('address')
-            ->add('city')
-            ->add('postalCode')
-            ->add('addressComplement')
+            ->add('name',TextType::class,
+                [
+                    'attr' =>
+                        [
+                            'class' => 'form-control',
+                            'placeholder' => 'Nom de la room'
+                        ]
+                ])
+            ->add('address',TextType::class,
+                [
+                    'attr' =>
+                        [
+                            'class' => 'form-control',
+                            'placeholder' => 'Addresse'
+                        ]
+                ])
+            ->add('city',TextType::class,
+                [
+                    'attr' =>
+                        [
+                            'class' => 'form-control',
+                            'placeholder' => 'Ville'
+                        ],
+                ])
+            ->add('postalCode',TextType::class,
+                [
+                    'attr' =>
+                        [
+                            'class' => 'form-control',
+                            'placeholder' => 'Code postal'
+                        ]
+                ])
+            ->add('addressComplement',TextType::class,
+                [
+                    'attr' =>
+                        [
+                            'class' => 'form-control',
+                            'placeholder' => 'Complement d\'addresse'
+                        ]
+                ])
         ;
     }
     
